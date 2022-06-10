c6415fcd5109d55f58b30c7abf32dfcbae37b219
anuzza <anuja3580@gmail.com>
2022-06-10 01:54:36 -0500

    item added to list

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
+import MealItem from './MealItem';
 
 
 const DUMMY_MEALS = [
@@ -30,12 +31,18 @@ const DUMMY_MEALS = [
 ];
 const AvailableMeals = ()=>{
 
-    const mealsList = DUMMY_MEALS.map(meal =><li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) =><MealItem 
+    key = {meal.id} 
+    name = {meal.name}
+    description = {meal.description} 
+    price = {meal.price}
+    />);
+
     return (
         <section className={classes.meals}>
            <Card>
                <ul>
-                {mealsList}
+               {mealsList}
 
                 </ul>
             </Card> 
--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -4,17 +4,17 @@ import react from 'react';
 
 const MealItem = props=>{
 
-    const price = `$${props.pricce.toFixed(2)}`;// to make sure it always renders two decimal plcaes
+    const price = `$${props.price.toFixed(2)}`;// to make sure it always renders two decimal plcaes
     return(
-        <li>
+        <li className={classes.meal}>
             <div>
                 <h3>{props.name}</h3>
-                <div>{props.description}</div>
-                <div>{price}</div>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
             
             </div>
             <div>
-                
+
             </div>
         </li>
 
