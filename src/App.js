a81f2df5eaf873a51c89a9f41a9f5eaaa20ba06c
anuzza <anuja3580@gmail.com>
2022-06-10 01:12:09 -0500

    added meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header";
 import react, {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
    
   );
