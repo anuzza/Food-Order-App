280ab27cb58c7e746e27ceeb6523a88541a6c862
anuzza <anuja3580@gmail.com>
2022-06-11 06:59:30 -0500

    made item removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -32,17 +32,39 @@ const cartReducer =(state, action)=>{
             updatedItems= state.item.concat(action.item);
 
         }      
-        
+             
        
         
-        return {
+    return {
             item : updatedItems,
             totalAmount: updatedTotalAmount
-
-
         };
-
     }
+    
+    if(action.type==='REMOVE'){
+        
+        
+        const existingCartItemIndex = state.item.findIndex((item)=>item.id===action.id);
+
+        const existingItem = state.item[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount-existingItem.price;
+
+        let updatedItems;
+        if(existingItem.amount===1){
+            updatedItems = state.item.filter(item=>item.id !== action.id);
+
+        }else{
+            const updatedItem = {...existingItem, amount: existingItem.amount-1};
+            updatedItems = [...state.item];
+            updatedItems[existingCartItemIndex]=updatedItem;
+
+        }
+
+
+    };
+
+    
 
 
 return defaultCartState;
