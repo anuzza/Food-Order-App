98a24fe26e0049d3ad47b280c15e81e52686acfe
anuzza <anuja3580@gmail.com>
2022-06-11 02:53:35 -0500

    created cart-context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ const Cart =props=>{
     const cartItems = <ul className={classes['cart-items']}>
         {[{id:'c1', name: 'Sushi', amount: 2, prices: 12.00},].map((items)=> <li> {items.name}</li>)}
         </ul>
-    return <Modal>
+    return <Modal onClose = {props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -13,7 +13,7 @@ const Cart =props=>{
         </div>
         <div className={classes.actions}>
             
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
 
             <button className={classes.button}>Order</button>
             
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import classes from './Modal.module.css';
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 const Backdrop = props =>{
-    return <div className={classes.backdrop}/>;
+    return <div className={classes.backdrop} onClick={props.onClose}/>;
 
 };
 
@@ -21,7 +21,7 @@ const Modal =(props)=>{
 
     return (
         <Fragment>
-        {ReactDOM.createPortal(<Backdrop/>, portalElement )} 
+        {ReactDOM.createPortal(<Backdrop onClose= {props.onClose}/>, portalElement )} 
         {ReactDOM.createPortal(<ModelOverlay>{props.children}</ModelOverlay>, portalElement)}
         </Fragment>
 
