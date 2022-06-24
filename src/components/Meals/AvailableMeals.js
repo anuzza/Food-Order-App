82b45dcf6231b7c4e1a9075ac550171cb025ed84
anuzza <anuja3580@gmail.com>
2022-06-24 03:58:57 -0500

    fetched data from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,14 +1,50 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem';
+import { useEffect, useState } from 'react';
 
 
-const DUMMY_MEALS = [
-  
-];
+
 const AvailableMeals = ()=>{
 
-    const mealsList = DUMMY_MEALS.map((meal) =><MealItem 
+  const [meals, setMeals] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchMeals = async()=>{
+
+      const response = await fetch('https://react-http-76fdb-default-rtdb.firebaseio.com/meals.json');
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData){
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      }
+
+      setMeals(loadedMeals);
+
+
+
+    };
+
+    fetchMeals();
+
+    
+
+  }, []);
+
+  
+
+
+
+    const mealsList = meals.map((meal) =><MealItem 
     id = {meal.id}
     key = {meal.id} 
     name = {meal.name}
