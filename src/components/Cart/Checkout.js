426bc1dd46a48079955b8670b779e2cf5090fdf1
anuzza <anuja3580@gmail.com>
2022-06-25 22:05:26 -0500

    invalid classes

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -47,27 +47,36 @@ const Checkout = (props) => {
         
     };
 
+    const nameControlClasses=`${classes.control} ${formInputValidity.name? '': classes.invalid}`;
+
+    const streetControlClasses=`${classes.control} ${formInputValidity.street? '': classes.invalid}`;
+
+    const postalControlClasses=`${classes.control} ${formInputValidity.postal? '': classes.invalid}`;
+
+    const cityControlClasses=`${classes.control} ${formInputValidity.city? '': classes.invalid}`;
+
+
 
 
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' ref={nameInputRef}/>
         {!formInputValidity.name && <p>Please enter a valid name!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={streetControlClasses}>
         <label htmlFor='street'>Street</label>
         <input type='text' id='street' ref={streetInputRef} />
         {!formInputValidity.street && <p>Please enter a valid street!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={postalControlClasses}>
         <label htmlFor='postal'>Postal Code</label>
         <input type='text' id='postal' ref={postalInputRef}/>
         {!formInputValidity.postal && <p>Please enter a valid postal code!</p>}
       </div>
-      <div className={classes.control}>
+      <div className={cityControlClasses}>
         <label htmlFor='city'>City</label>
         <input type='text' id='city' ref={cityInputRef}/>
         {!formInputValidity.city && <p>Please enter a valid city!</p>}
